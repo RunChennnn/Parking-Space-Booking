ea3ea3dd1b9ca5411fe7fb0d21278ab560457819
General MA <kidmmz@hotmail.com>
2023-06-25 02:10:43 +1000

    firebase setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,52 @@
 import express from "express";
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, get } from 'firebase/database';
 
 const port = 3141
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, }));
+app.use(bodyParser.json());
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApBdX_ouOp0E9Bez09PtHyMa4UL-qDYBo",
+    authDomain: "room-e5563.firebaseapp.com",
+    projectId: "room-e5563",
+    storageBucket: "room-e5563.appspot.com",
+    messagingSenderId: "665563320009",
+    appId: "1:665563320009:web:2bff29562834fdf4bac718",
+    measurementId: "G-1NXQ4FX22V",
+    databaseURL: "https://room-e5563-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
+  
+
+const firebase = initializeApp(firebaseConfig);
+const db = getDatabase(firebase);
 
 app.get('/test', (req, res) => {
     console.log(req.body);
     console.log("responted to /test");
-    return res.json({ id: 2 });
+    return res.status(200).json({ id: 2 });
 });
+
+app.get('/testDB', async (req,res) => {
+    console.log(req)
+    const testDataRef = ref(db, '/testDatabaseCollection')
+    const snapshot = await get(testDataRef);
+    let respond;
+    console.log(snapshot.val());
+    if (snapshot.exists()){
+        respond = snapshot.val()
+    } else {
+        respond = "Nothing in the database"
+    }
+    console.log("All information retrieved from Database")
+    return res.status(200).json({respond});
+});
+
 
 const server = app.listen(port, () => {
     console.log(`Backend is now listening on port ${port}!`);
