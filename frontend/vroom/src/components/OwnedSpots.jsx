223bb2ac71dc7d15e4fc18a7ea0060b1fc60ac5e
Run <z5389839@ad.unsw.edu.au>
2023-06-26 17:28:18 +1000

    submit2

--- frontend/vroom/src/components/OwnedSpots.jsx
+++ frontend/vroom/src/components/OwnedSpots.jsx
@@ -1,6 +1,17 @@
 import React from "react"
 import {useNavigate, useParams} from 'react-router-dom'
-import {Table, Button, Typography} from '@mui/material'
+import {
+    Table,
+    Button,
+    Typography,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    TablePagination, TableFooter
+} from '@mui/material'
+import Paper from '@mui/material/Paper'
 import SpotNavigationBar from './SpotNavigationBar'
 import makeRequest from "../utilities/makeRequest";
 
@@ -15,9 +26,19 @@ function OwnedSpots () {
         margin: '20px 30% 0 30%'
     }
 
+    // data tested for ui layout
+    function initData() {
 
+    }
+    const testData = [
 
-    const itemColumn = [{},{},{},{}]
+    ]
+    const rows = testData
+
+    function toUpdateSpot() {
+
+    }
+
 
     function toRegisterSpot() {
         navigate('/spots/new')
@@ -28,7 +49,50 @@ function OwnedSpots () {
             <div>
               <SpotNavigationBar />
               <Typography align="center" variant="h4">Owned Spots</Typography>
-              <Table />
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">Name</TableCell>
+                                <TableCell align="center">Address</TableCell>
+                                <TableCell align="center">Average Price per hour</TableCell>
+                                <TableCell align="center">Demand-driven pricing</TableCell>
+                                <TableCell align="center">Edit</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.address}</TableCell>
+                                    <TableCell align="right">{row.averagePrice}</TableCell>
+                                    <TableCell align="right">{row.demandDriven}</TableCell>
+                                    <TableCell align="right"><Button variant="contained">Edit</Button></TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                    colSpan={3}
+                                    count={rows.length}
+                                    SelectProps={{
+                                        inputProps: {
+                                            'aria-label': 'rows per page',
+                                        },
+                                        native: true,
+                                    }}
+                                />
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </TableContainer>
               <Button variant="contained" style={buttonStyle} onClick={toRegisterSpot}>Register new spot</Button>
             </div>
         </>
