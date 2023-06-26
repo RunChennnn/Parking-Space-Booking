ec614c71590b014d92e0ae012e378a317eaa503b
Run <z5389839@ad.unsw.edu.au>
2023-06-26 16:38:24 +1000

    submit1

--- frontend/vroom/src/components/NewSpot.jsx
+++ frontend/vroom/src/components/NewSpot.jsx
@@ -1,12 +1,25 @@
 import React from "react"
+import SpotNavigationBar from "./SpotNavigationBar";
+import {useParams} from "react-router-dom";
+import makeRequest from "../utilities/makeRequest";
+import {Button} from "@mui/material";
 
 function NewSpot () {
+
+    const params = useParams()
+
+    const buttonStyle = {
+        margin: '20px 30% 0 30%'
+    }
+
+    function confirmRegister() {
+
+    }
     
     return (
-        <>  
-            <p>
-                TODO new spot page
-            </p>
+        <>
+            <SpotNavigationBar />
+            <Button variant="contained" style={buttonStyle} onClick={confirmRegister}>Register</Button>
         </>
     )
 }
--- frontend/vroom/src/components/OwnedSpots.jsx
+++ frontend/vroom/src/components/OwnedSpots.jsx
@@ -1,15 +1,36 @@
 import React from "react"
-import { useParams } from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
+import {Table, Button, Typography} from '@mui/material'
+import SpotNavigationBar from './SpotNavigationBar'
+import makeRequest from "../utilities/makeRequest";
+
 
 function OwnedSpots () {
 
     const params = useParams()
+
+    const navigate = useNavigate()
+    
+    const buttonStyle = {
+        margin: '20px 30% 0 30%'
+    }
+
+
+
+    const itemColumn = [{},{},{},{}]
+
+    function toRegisterSpot() {
+        navigate('/spots/new')
+    }
     
     return (
         <>  
-            <p>
-                TODO owned spots page (for user id {params.userID})
-            </p>
+            <div>
+              <SpotNavigationBar />
+              <Typography align="center" variant="h4">Owned Spots</Typography>
+              <Table />
+              <Button variant="contained" style={buttonStyle} onClick={toRegisterSpot}>Register new spot</Button>
+            </div>
         </>
     )
 }
