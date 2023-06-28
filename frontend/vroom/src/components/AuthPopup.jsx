bad29fc3c56d25aeea259cca9c7cd2eb638c8830
Darcy Raimond <darcy.raimond@gmail.com>
2023-06-28 19:51:56 +1000

    more auth changes

--- frontend/vroom/src/components/AuthPopup.jsx
+++ frontend/vroom/src/components/AuthPopup.jsx
@@ -20,7 +20,7 @@ function AuthPopup (props) {
   };
 
   async function onConfirm () {
-    const preresponse = await makeRequest('GET', `user/${params.userID}`, {});
+    const preresponse = await makeRequest('GET', `user/${localStorage.getItem('vroom-id')}`, {});
     const response = await makeRequest('POST', `auth/${localStorage.getItem('vroom-id')}`, { email: preresponse.email, password: password });
     if (!response.error) {
       props.onConfirm();
