61c787fcfcdb0377ea495730baa88dcd864cf6f2
Darcy Raimond <darcy.raimond@gmail.com>
2023-07-15 21:56:04 +1000

    merged

--- frontend/vroom/src/components/SpotUseRenter.jsx
+++ frontend/vroom/src/components/SpotUseRenter.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import makeRequest from "../utilities/makeRequest";
 import {useNavigate} from "react-router-dom";
@@ -8,4 +9,24 @@ function SpotUseRenter() {
         <></>
     )
 }
+=======
+import React from "react"
+import NavigationBar from "./NavigationBar"
+import { useParams } from "react-router-dom"
+import {useNavigate} from "react-router-dom";
+
+function SpotUseRenter() {
+  const navigate = useNavigate()
+
+  const params = useParams();
+    
+  return (
+    <>
+      <NavigationBar />
+      Spot use page for booking with ID {params.bookingID} from the perspective of the renter
+    </>
+  )
+}
+
+>>>>>>> 5832bb6 (merged run's stuff)
 export default SpotUseRenter
