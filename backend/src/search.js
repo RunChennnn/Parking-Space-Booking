853a8e9f5a9192008faf7da9e2a83419a412cf2e
General MA <kidmmz@hotmail.com>
2023-07-19 15:32:09 +1000

    recommend implemented

--- backend/src/search.js
+++ backend/src/search.js
@@ -1,14 +1,82 @@
 import { db } from "./firebaseConfig.js";
 
+const calculateSimilarityScores = async (spotData, refSpotsData) => {
+  let similarityScore = 0
+  // I declare all fields explicitly here on purpose to make it clear what is used in recommend criteria
+  const streetName = spotData.streetName;
+  const streetNumber = spotData.streetNumber
+  const suburb = spotData.suburb;
+  const owner = spotData.owner;
+  const evCharging = spotData.evCharging;
+  const largestVehicle = spotData.largestVehicle;
+  const disabledAccess = spotData.disabledAccess;
+  const clearance = spotData.clearance;
+  const basePrice = spotData.basePrice;
+
+  refSpotsData.forEach( refSpot => {
+    if (streetName ===  refSpot.streetName) {
+      similarityScore += 3
+    }
+    if (suburb ===  refSpot.suburb) {
+      similarityScore += 1
+    }
+    if (streetNumber ===  refSpot.streetNumber) {
+      similarityScore += 1
+    }
+    if (owner ===  refSpot.owner) {
+      similarityScore += 1
+    }
+    if (evCharging ===  refSpot.evCharging) {
+      similarityScore += 8
+    }
+    if (largestVehicle ===  refSpot.largestVehicle) {
+      similarityScore += 1
+    }
+    if (disabledAccess ===  refSpot.disabledAccess) {
+      similarityScore += 5
+    }
+    if (clearance >=  refSpot.clearance) {
+      similarityScore += 3
+    }
+    if (basePrice >=  refSpot.basePrice) {
+      const priceSimilarity = Math.pow(refSpot.basePrice/basePrice, 2)*2
+      similarityScore += priceSimilarity
+    }
+    if (basePrice <  refSpot.basePrice) {
+      const priceSimilarity = Math.pow(basePrice/refSpot.basePrice, 4)*2
+      similarityScore += priceSimilarity
+    }
+  })
+  return similarityScore
+}
+
+const evaluateAndSort = async (potentialSpots, previousSpotsData ) => {
+  let spotScores = [];
+  potentialSpots.forEach(async spot => {
+    spotScores.push({id:spot.id, score: await calculateSimilarityScores(spot.data(), previousSpotsData)})
+    spotScores.sort((a,b)=> b.score - a.score)
+  })
+  return spotScores;
+}
+
 const recommendSpot = async (uid, num, alreadyReceived) => {
     try {
       let spotList = []
-      const spotRef = db.collection('Spots');
-      const snapshot = await spotRef.get();
-  
-      if (!snapshot.empty) {
+      const previousBookings = await db.collection('Bookings').where('userID', '==', uid).limit(5).get();
+      let previousSpotsData = []
+      previousBookings.forEach(async (booking) => {
+        const sid = booking.data().spotID;
+        const spotData = await db.collection('Spots').doc(sid).get();
+        previousSpotsData.push(spotData.data())
+      })
+
+      const spotRef = db.collection('Spots').where('owner', '!=', uid);
+      const potentialSpots = await spotRef.get();
+      let spotScores = await evaluateAndSort(potentialSpots, previousSpotsData);
+
+      if (!potentialSpots.empty) {
         let count = 0 
-        snapshot.forEach(spot => {
+        spotScores.forEach(spot => {
             if (count < num && !alreadyReceived.includes(spot.id)) {
                 spotList.push(spot.id);
                 count ++;
@@ -19,6 +87,7 @@ const recommendSpot = async (uid, num, alreadyReceived) => {
           status: 200,
           message: `${spotList.length} Spots successfully recommended`,
           ids: spotList,
+          scores: spotScores,
         };
       } else {
         console.log(`Database is empty, unable to give recommendation`);
