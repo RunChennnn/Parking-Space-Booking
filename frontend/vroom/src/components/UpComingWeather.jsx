9df136cb48fc8aa59cedcdab54e7615132d9529e
Run <z5389839@ad.unsw.edu.au>
2023-07-30 18:13:40 +1000

    error update upcoming weather

--- frontend/vroom/src/components/UpComingWeather.jsx
+++ frontend/vroom/src/components/UpComingWeather.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
-function UpComingWeather () {
+import { Box } from '@mui/material';
+function UpComingWeather (props) {
   return (
         <>
-
+          <Box sx={{
+            maxWidth: 1200,
+            height: 300,
+            display: 'flex',
+            justifyContent: 'flex-start',
+            p: 1,
+            m: 1,
+            borderRadius: 1,
+          }}>
+            {console.log(props.weathers.summary[0])}
+            {console.log(props.weathers.summary[1])}
+            {console.log(props.weathers.low[0])}
+            {console.log(props.weathers.high[1])}
+          </Box>
         </>
   )
 }
