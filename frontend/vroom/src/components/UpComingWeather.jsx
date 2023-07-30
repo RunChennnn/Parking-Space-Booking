66b4858c44c191e618664f539d457953a887ad91
Run <z5389839@ad.unsw.edu.au>
2023-07-30 20:54:10 +1000

    upcoming weather werid data

--- frontend/vroom/src/components/UpComingWeather.jsx
+++ frontend/vroom/src/components/UpComingWeather.jsx
@@ -1,21 +1,66 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, ImageListItem, ImageListItemBar } from '@mui/material';
+import clear from '../static/clear.png'
+import partlyCloudy from '../static/partly-cloudy.png'
+import cloudy from '../static/cloudy.png'
+import showers from '../static/showers.png'
+import snowShowers from '../static/snowshowers.png'
+import rain from '../static/rain.png'
+import snow from '../static/snow.png'
+import thunderstorm from '../static/thunderstorm.png'
+import dayjs from 'dayjs';
 function UpComingWeather (props) {
+  const weatherIcons = {
+    Clear: clear,
+    'Partly Cloudy': partlyCloudy,
+    Cloudy: cloudy,
+    Showers: showers,
+    'Snow Showers': snowShowers,
+    Rain: rain,
+    Snow: snow,
+    Thunderstorm: thunderstorm
+  }
+
+  const indexOfTemp = (tmp) => {
+    return props.weathers.summary.indexOf(tmp)
+  }
+
+  const loadLowTemp = (tmp) => {
+    return props.weathers.low[indexOfTemp(tmp)]
+  }
+
+  const loadHighTemp = (tmp) => {
+    return props.weathers.high[indexOfTemp(tmp)]
+  }
+
+  const loadUpcomingDates = (tmp) => {
+    const idx = indexOfTemp(tmp)
+    return dayjs().add(idx, 'day').format('MMM D, YYYY')
+  }
+
   return (
         <>
           <Box sx={{
             maxWidth: 1200,
-            height: 300,
+            height: 100,
             display: 'flex',
             justifyContent: 'flex-start',
             p: 1,
             m: 1,
-            borderRadius: 1,
+            borderRadius: 2.5,
+            margin: '0 0 50px 0 '
           }}>
-            {console.log(props.weathers.summary[0])}
-            {console.log(props.weathers.summary[1])}
-            {console.log(props.weathers.low[0])}
-            {console.log(props.weathers.high[1])}
+            {props.weathers.summary.map((tmp) =>
+                <ImageListItem key={tmp} sx={{ height: 80 }}>
+                  <img src={weatherIcons[tmp]} />
+                  <ImageListItemBar
+                    title={`${loadLowTemp(tmp)}°C/${loadHighTemp(tmp)}°C`}
+                    subtitle={loadUpcomingDates(tmp)}
+                    position="below"
+                    align="center"
+                  />
+                </ImageListItem>
+            )}
           </Box>
         </>
   )
